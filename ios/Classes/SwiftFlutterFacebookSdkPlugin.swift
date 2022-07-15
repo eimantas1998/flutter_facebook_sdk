a83b148d03d2e9cb0b09eb602b183dbb60bd98eb
Rimantovas <rimas.pov@gmail.com>
2022-07-15 17:12:04 +0300

    more fixes

--- ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
+++ ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
@@ -146,7 +146,7 @@ public class SwiftFlutterFacebookSdkPlugin: NSObject, FlutterPlugin, FlutterStre
         AppEvents.shared.logEvent(.initiatedCheckout, valueToSum: totalPrice, parameters: parameters)
     }
     
-    func logGenericEvent(args: Dictionary<String, Any>){
+    func logGenericEvent(args: [AppEvents.ParameterName: Any]){
         let eventName = args["eventName"] as! String
         let valueToSum = args["valueToSum"] as? Double
         let parameters = args["parameters"] as? Dictionary<AppEvents.ParameterName, Any>
@@ -230,7 +230,7 @@ public class SwiftFlutterFacebookSdkPlugin: NSObject, FlutterPlugin, FlutterStre
             if let myArgs = args as? [String: Any],
                let amount = myArgs["amount"] as? Double,
                let currency = myArgs["currency"] as? String,
-               let parameters = myArgs["parameters"] as? Dictionary<String, Any>{
+               let parameters = myArgs["parameters"] as? [AppEvents.ParameterName: Any]{
                 self.logPurchase(amount: amount, currency: currency, parameters: parameters)
                 result(true)
                 return
