eaa90a1d31ecc8ba37105e44e3444ba5cfbd8f74
Rimantovas <rimas.pov@gmail.com>
2022-07-19 15:09:58 +0300

    giggles

--- ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
+++ ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
@@ -149,13 +149,13 @@ public class SwiftFlutterFacebookSdkPlugin: NSObject, FlutterPlugin, FlutterStre
     func logGenericEvent(args: [String: Any]){
         let eventName = args["eventName"] as! String
         let valueToSum = args["valueToSum"] as? Double
-        let parameters = args["parameters"] as? Dictionary<AppEvents.ParameterName, Any>
+        let parameters = args["parameters"] as? [AppEvents.ParameterName: Any]
         if(valueToSum != nil && parameters != nil){
-            AppEvents.shared.logEvent(AppEvents.Name(eventName), valueToSum: valueToSum!, parameters: parameters!)
+            AppEvents.shared.logEvent(AppEvents.Name(eventName), valueToSum: valueToSum, parameters: parameters)
         }else if(parameters != nil){
-            AppEvents.shared.logEvent(AppEvents.Name(eventName), parameters: parameters!)
+            AppEvents.shared.logEvent(AppEvents.Name(eventName), parameters: parameters)
         }else if(valueToSum != nil){
-            AppEvents.shared.logEvent(AppEvents.Name(eventName), valueToSum: valueToSum!)
+            AppEvents.shared.logEvent(AppEvents.Name(eventName), valueToSum: valueToSum)
         }else{
             AppEvents.shared.logEvent(AppEvents.Name(eventName))
         }
@@ -230,7 +230,7 @@ public class SwiftFlutterFacebookSdkPlugin: NSObject, FlutterPlugin, FlutterStre
             if let myArgs = args as? [String: Any],
                let amount = myArgs["amount"] as? Double,
                let currency = myArgs["currency"] as? String,
-               let parameters = myArgs["parameters"] as? Dictionary<AppEvents.ParameterName, Any>
+               let parameters = myArgs["parameters"] as? [AppEvents.ParameterName: Any]
                 {
                 self.logPurchase(amount: amount, currency: currency, parameters: parameters)
                 result(true)
